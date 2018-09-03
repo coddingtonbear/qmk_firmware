c059c8a6378ee4ab34f7cb35b9b85a9bb88e1a1b
Adam Coddington <me@adamcoddington.net>
2018-09-02 17:36:51 -0700

    Adding definition allowing one to enable/disable PSP1000 support.

--- keyboards/ts65avr/keymaps/coddingtonbear/keymap.c
+++ keyboards/ts65avr/keymaps/coddingtonbear/keymap.c
@@ -22,6 +22,7 @@
 #define LT_MSE LT(_MOUSE, KC_SCLN)
 #define TG_GAME TG(_GAME)
 
+//#define ENABLE_PSP1000_MOUSE
 #define MOUSE_MOVE_UNIT 10
 #define MOUSE_MOVE_FINE_UNIT 2
 #define MOUSE_NULL_ZONE 0.
@@ -183,6 +184,7 @@ void matrix_init_user(void) {
 }
 
 void matrix_scan_user(void) {
+#ifdef ENABLE_PSP1000_MOUSE
   int xMouse = analogRead(X_MOUSE_PIN);
   int yMouse = analogRead(Y_MOUSE_PIN);
   report_mouse_t currentReport = pointing_device_get_report();
@@ -252,4 +254,5 @@ void matrix_scan_user(void) {
     currentReport.x = value;
   }
   pointing_device_set_report(currentReport);
+#endif
 }
