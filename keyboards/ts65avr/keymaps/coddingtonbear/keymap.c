25a317bcd2b8b1c511078aee3251d5ba613dd331
Adam Coddington <me@adamcoddington.net>
2018-09-26 16:03:10 -0700

    Make the emoji layer key send '+' if tapped.

--- keyboards/ts65avr/keymaps/coddingtonbear/keymap.c
+++ keyboards/ts65avr/keymaps/coddingtonbear/keymap.c
@@ -20,7 +20,7 @@
 #define MO_FUNC MO(_FUNC)
 #define MO_FNC2 MO(_FUNC2)
 #define MO_MSE MO(_MOUSE)
-#define MO_EMOJI MO(_EMOJI)
+#define LT_EMOJI LT(_EMOJI, KC_PPLS)
 #define LT_MSE LT(_MOUSE, KC_SCLN)
 #define TG_GAME TG(_GAME)
 #define KC_CHOM RCTL(KC_HOME)
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC, KC_1,    KC_2,    KC_3,     KC_4,    KC_5,   KC_6,    KC_7,   KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NONE, KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,            KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, \
         KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,    KC_G,            KC_H,   KC_J,     KC_K,    KC_L,    LT_MSE,  KC_QUOT, KC_NONE, KC_ENT,  KC_PGDN, \
-        KC_LSFT, KC_NONE, KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,    KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   MO_EMOJI, \
+        KC_LSFT, KC_NONE, KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,    KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   LT_EMOJI, \
         MO_FUNC, KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC,                   KC_SPC,           MO_FNC2, KC_RALT, KC_RCTL, KC_LEFT,          KC_DOWN, KC_RIGHT),
 
     [_FUNC] = KEYMAP( \
